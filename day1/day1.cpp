61bc0e50c79f449b7c2b12337405c14cef330fac
Erik Mansén <e.naaru@hotmail.com>
2020-12-01 18:37:55 +0100

    Implement day 1 solution

---
+++ day1/day1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+    set<int> entries;
+
+    //Read input.txt
+    
+    string input;
+    ifstream file ("input.txt");
+
+    if (file.is_open()) {
+        while(getline(file, input)){
+            entries.insert(stoi(input));
+        }
+        file.close();
+    }
+    
+    int twoValueExpanseReport;
+    vector<int> temp;
+    for(auto it = entries.begin(); it != entries.end(); ++it){
+        //Find sought-after value.
+ 
+        if(entries.find(2020 - *it) != entries.end()) {
+            twoValueExpanseReport = (2020 - *it) * (*it);
+        }
+    }
+
+    int threeValueExpanseReport;
+    for(auto first = entries.begin(); first != entries.end(); ++first) {
+        for (auto second = next(first); second != entries.end(); ++second){
+
+            if(entries.find(2020 - *first - *second) != entries.end()) {
+                threeValueExpanseReport = (2020 - *first - *second) * (*first) * (*second);
+            }
+        }
+    }
+
+    
+    std::cout << "Two-entry expense report: " << twoValueExpanseReport << endl;
+    std::cout << "Three-entry expanse report: " << threeValueExpanseReport << endl;
+}
