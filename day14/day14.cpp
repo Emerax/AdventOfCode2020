eb0fbd4cd2d8d1a659311ef32a96009f55fa3515
Erik Mansén <e.naaru@hotmail.com>
2020-12-14 11:39:23 +0200

    Implement day 14 part using quantum physics, sort of

--- day14/day14.cpp
+++ day14/day14.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <stdlib.h>
 #include <algorithm>
+#include <bitset>
 
 using namespace std;
 
@@ -23,7 +24,7 @@ vector<string> split(string s, string delimiter) {
     return result;
 }
 
-unsigned long apply_mask(unsigned long value, string mask) {
+unsigned long apply_mask_v1(unsigned long value, string mask) {
     for (int i{0}; i < mask.length(); ++i) {
         if (mask[i] == '0') {
             value &= ~(1UL << i);
@@ -35,11 +36,35 @@ unsigned long apply_mask(unsigned long value, string mask) {
     return value;
 }
 
+string apply_mask_v2(string s, string mask) {
+    for (int i{0}; i < mask.length(); ++i) {
+        if (mask[i] == '1') {
+            s.replace(i, 1, "1");
+        } else if (mask[i] == 'X') {
+            s.replace(i, 1, "X");
+        }
+    }
+
+    return s;
+}
+
+void collapse_wave_function(string quantum_adress, vector<unsigned long> &adresses) {
+    auto x_pos = quantum_adress.find("X");
+    if (x_pos == string::npos) {
+        reverse(quantum_adress.begin(), quantum_adress.end());
+        adresses.push_back(bitset<64>(quantum_adress).to_ullong());
+    } else {
+        collapse_wave_function(quantum_adress.replace(x_pos, 1, "0"), adresses);
+        collapse_wave_function(quantum_adress.replace(x_pos, 1, "1"), adresses);
+    }
+} 
+
 int main() {
     ifstream file ("input.txt");
     string input{};
 
-    map<unsigned long, unsigned long> mem{};
+    map<unsigned long, unsigned long> mem1{};
+    map<unsigned long, unsigned long> mem2{};
     string mask;
 
     if (file.is_open()) {
@@ -55,16 +80,34 @@ int main() {
                 unsigned long adress = stoul(instruction[0].substr(adressStart, adressEnd - adressStart));
                 unsigned long raw = stoul(instruction[2]);
 
-                mem[adress] = apply_mask(raw, mask);
+                mem1[adress] = apply_mask_v1(raw, mask);
+                
+                string quantum_adress = bitset<64>(adress).to_string();
+                reverse(quantum_adress.begin(), quantum_adress.end());
+                quantum_adress = apply_mask_v2(quantum_adress, mask);
+                vector<unsigned long> adresses{};
+                collapse_wave_function(quantum_adress, adresses);
+
+
+
+                for (auto adress: adresses) {
+                    mem2[adress] = raw;
+                }
             }
         }
         file.close();
 
-        unsigned long sum{0U};
-        for (auto val : mem) {
-            sum += val.second;
+        unsigned long sum_v1{0U};
+        for (auto val : mem1) {
+            sum_v1 += val.second;
         }
 
-        cout << "Sum is " << sum << endl;
+        unsigned long sum_v2{0U};
+        for (auto val: mem2) {
+            sum_v2 += val.second;
+        }
+
+        cout << "Version 1 sum is " << sum_v1 << endl;
+        cout << "Version 2 sum is " << sum_v2 << endl;
     }
 }
